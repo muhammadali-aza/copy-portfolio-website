992c21fe2dae942799e74d959483704800b5700f
muhammadali-aza <alirazaitservice@gmail.com>
2026-04-07 14:56:16 +0500

    connect backend

--- src/navbar/pages/home/ContactSection/ContactSection.jsx
+++ src/navbar/pages/home/ContactSection/ContactSection.jsx
@@ -34,7 +34,7 @@ export default function ContactSection() {
                 message: formData.message,
             };
 
-            const response = await fetch("https://portfolio-website-backend.railway.app/api/form/submit", {
+            const response = await fetch("https://portfolio-website-backend-production-6805.up.railway.app/api/form/submit", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
